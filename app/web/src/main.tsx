de7a6b5404af7a10213905e5abc79117832fee95
Hassan <hassank07@mail.ru>
2026-05-14 15:52:05 +0300

    Update main.tsx

--- app/web/src/main.tsx
+++ app/web/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import App from './app/App.tsx'
 import "./main.css"
 import GlobalProvider from './app/provider/GlobalProvider/ui/GlobalProvider.tsx';
+import bridge from '@vkontakte/vk-bridge';
 
+
+bridge.send("VKWebAppInit");
 
 createRoot(document.getElementById("root")!).render(
   <GlobalProvider>
